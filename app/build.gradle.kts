eedd3346c62831a952a4910ed1720cee4f267da8
shrx <differenxe@gmail.com>
2026-06-09 14:15:32 +0200

    Suppress preview window to prevent white flash on activity transitions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.shrx.calalarm"
         minSdk = 34
         targetSdk = 35
-        versionCode = 55
-        versionName = "0.9.8"
+        versionCode = 56
+        versionName = "0.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
